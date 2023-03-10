c6badb579ad04d4b564f76a405951c77d9732bb9
Andrei Piatsevich <apiats2@gmail.com>
2023-03-10 12:14:29 +0300

    add Sort class

--- src/main/java/com/piatsevich/utils/Sort.java
+++ src/main/java/com/piatsevich/utils/Sort.java
@@ -1,10 +1,9 @@
-package utils;
+package com.piatsevich.utils;
 
 import com.piatsevich.comparator.GeneralComparator;
 
-import java.util.Comparator;
 import java.util.List;
 
 public interface Sort {
-    public<T> void sort(List<T> ballList, GeneralComparator<T> cpm);
+    <T> void sort(List<T> ballList, GeneralComparator<T> cpm);
 }
